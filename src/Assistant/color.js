72469d826a838402b424ee621971f8825be78985
Mahmoud Almadhoun <mahmoud.talat899@gmail.com>
2022-05-16 11:43:01 +0200

    up

---
+++ src/Assistant/color.js
@@ -0,0 +1,24 @@
+const COLORS = {
+    firstColor: '#ff3b2f',
+    lastColor: '#f3f5f8',
+    primary: '#fff',
+    secondary: '#ff3b2f',
+    dark: 'red',
+    light: 'black',
+    lightless: '#00000057',
+    background: 'red',
+    xpColor: '#FF392F34',
+    white : 'white',
+    firstRed : '#ff3b2f85',
+    notClick : '#ff3b2f30',
+    find: '#eee',
+    finds : '#f3f6fa',
+    val : '#ff3b2f2e',
+    op : '#00000010',
+    green : '#198754',
+    andraColor : '#ff3b2f12',
+    change : '#FEE3EC',
+    testing : 'rgba(255, 59, 47, 0.32)'
+  };
+  
+  export default COLORS;
